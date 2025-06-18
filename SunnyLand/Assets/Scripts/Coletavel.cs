2a46dbb78e964a1e032969a4d5466b51cd6b5db5
lucassilb <lucassilveribomtempo@gmail.com>
2025-06-18 12:08:48 -0300

    moeda

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coletavel : MonoBehaviour
 {
+    public ParticleSystem efeito;
+    public TMP_Text texto_pontuacao;
+    int points = 0;
     private int pontos = 5;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,17 @@ public class Coletavel : MonoBehaviour
       //  Debug.Log("O Jogo está rodando!");
     }
     
-  
+  private void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.CompareTag("Player")){ 
+            Debug.Log("Bateu");
+            outro.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            Instantiate(efeito, transform.position, Quaternion.identity);
+            points++;
+            texto_pontuacao.text = "Pontos" + points.ToString();
+            Destroy(gameObject);
+
+        }
+    }
 
 }
